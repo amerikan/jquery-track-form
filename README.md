815ae6d157fa6366f76bd73696307d46599b3ca2
Erik Montes <zomberik@gmail.com>
2015-03-31 00:08:07 -0700

    fixes, docs, and demo
    - fixed plugin issues
    - added usage documentation
    - added demo page

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # jquery-trackform.js
 A jQuery plugin to track if form values have changed
 
+[Demo](http://amerikan.github.io/jquery-track-form/)
+
 ## Usage
 
+In your HTML add an unique class to the form elements you would like to keep track for changes.
 ```html
 
-<input value="hello" class="track-form" />
-
-<textarea class="track-form">yo!</textarea>
+	<input value="hello" class="track-form" />
+	<textarea class="track-form">yo!</textarea>
 
 ```
 
+In your javascript initialize by selecting 
+
 ```js
 $(function () {
+	
+	// Track any element with the given class
+	$(.track-form').trackForm();
 
-  $('input, textarea').trackForm();
-
+	// or you can be specific to input/textarea elements
+	$('input.track-form, textarea.track-form').trackForm();
 });
 ```
 
--- jquery.trackform.js
+++ jquery.trackform.js
@@ -1,43 +1,35 @@
 /*
 * jquery.trackform.js - A jQuery plugin to track if initial form values have changed
-* Version: 0.0.1
 * (c) 2015 Erik Montes
 * MIT license
 */
 
 $(function () {
 
-    $.fn.trackFrom = function(options) {
-
-        // default settings:
-        var defaults = {
-            customClassName: 'form-tracker'
-        };
-
-        var settings = $.extend({}, defaults, options);
+    $.fn.trackForm = function() {
 
         var data = [];
         var dirtyInputs = [];
         var isDirty = false;
 
         // Only track inputs with the specified class that are not hidden
-        $('textarea' + '.' + customClassName + ', input' + '.' + customClassName).not(':hidden').each(function(i) {
+        return this.each(function (i) {
             
             var $this = $(this);
 
-            // Save initial value
+            // Save initial input value
             data[i] = $this.val();
 
             // Add an ID to track independently
-            $this.data(customClassName +'-id', i);
+            $this.data('trackform-id', i);
 
-            // Not dirty by default
+            // Input not dirty by default
             dirtyInputs[i] = false;
 
         }).change(function () {
 
             var $this = $(this);
-            var id = $this.data(customClassName +'-id');
+            var id = $this.data('trackform-id');
 
             // Check if value differs from initial value
             if (data[id] !== $this.val()) {
@@ -52,7 +44,7 @@ $(function () {
                 isDirty = true;
 
                 window.onbeforeunload = function () {
-                    return 'Are you sure you want to leave without saving?';
+                    return 'Are you sure you want to leave without saving the changes?';
                 };
 
             } else {
